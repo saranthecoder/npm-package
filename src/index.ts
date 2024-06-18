d5138e2d83dae4fa03ae7efb4bf57e9ea824cea1
saranthecoder <115061220+saranthecoder@users.noreply.github.com>
2024-06-18 15:52:58 +0530

    index update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * from './functions';
 export * from './types';
 
+
+//add function by default access
 export default function sum(a: number, b: number): number {
     // ... your sum function implementation
     return a + b;
